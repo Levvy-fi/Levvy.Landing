140cb2bfbb68c82b1f498fc3fa3576b0efdead7b
KeeeeEf <kfrgallego@gmail.com>
2025-06-20 03:04:03 +0800

    feat: implement section 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import MainLayout from "../components/common/main-layout"
 import Section6 from "../components/sections/section6"
+import Section3 from "../components/sections/section3"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <MainLayout>
+      <Section3/>
       <Section6/>
     </MainLayout>
   )
