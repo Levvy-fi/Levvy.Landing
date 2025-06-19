83a7511fd1294f2b382b2e55d48abc3c8385a274
KeeeeEf <kfrgallego@gmail.com>
2025-06-20 01:51:15 +0800

    feat: implement section 6

--- src/components/common/main-layout.tsx
+++ src/components/common/main-layout.tsx
@@ -7,16 +7,21 @@ declare module "@mui/material/styles" {
 const theme = createTheme({
     palette: {
         background: {
-            default: "#12100F",
-            paper: "#201B13"
+          default: "#12100F",
+          paper: "#201B13"
         },
         primary: {
           light: "#FFD89F",
           main: "#F9B63E",
           dark: "#844C1F"
         },
+        secondary: {
+          light: "#FFEFD2",
+          main: "#FFD78E"
+        },
         text: {
-          primary: "#EDE1D3"
+          primary: "#EDE1D3",
+          secondary: "#6C4900"
         }
     },
     typography: {
