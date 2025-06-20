32f99ff965be0ea45b4ab4dd47ec0cb6ec2c4972
CML90 <caitlincebu@hotmail.com>
2025-06-21 03:37:09 +0800

    feat: implement section 3 responsiveness

--- src/components/sections/section3.tsx
+++ src/components/sections/section3.tsx
@@ -11,18 +11,18 @@ const Section3: React.FC = () => {
     return (
         <section 
             id="section3"
-            className="h-267 flex items-center relative"
+            className="h-267 flex items-center relative overflow-hidden"
         >
             <div className="container mx-auto flex flex-col items-center justify-center">
                 <div className="absolute flex items-center">
                     <div 
-                        className="w-220 h-190 rounded-full !opacity-20 !blur-3xl"
+                        className="w-20 sm:w-50 md:w-80 lg:w-120 xl:w-180 2xl:w-220 h-190 rounded-full !opacity-20 !blur-3xl"
                         style={{
                             backgroundImage: `linear-gradient(to right, ${theme.palette.gradient.levvy.main}, ${theme.palette.gradient.levvy.main_light})`
                         }}
                     />
                     <div 
-                        className="w-100 h-100 rounded-r-full !opacity-20 !blur-3xl"
+                        className="w-20 sm:w-50 :md:w-80 lg:w-120 xl:w-180 2xl:w-220 h-100 rounded-r-full !opacity-20 !blur-3xl"
                         style={{
                             backgroundImage: `linear-gradient(to right, ${theme.palette.gradient.levvy.secondary}, ${theme.palette.gradient.levvy.secondary_light})`
                         }}
@@ -31,45 +31,44 @@ const Section3: React.FC = () => {
                 <div className="flex flex-col items-center justify-center">
                     <Typography 
                         sx={{
-                            fontSize: 24,
                             fontWeight: 600,
                             color: theme.palette.primary.light
                         }}
+                        className="!text-base !mb-2 lg:mb-0 lg:!text-6"
                     >
                         Coming Soon...
                     </Typography>
                     <img
                         src={LevvyLogo}
                         alt="levvy logo"
-                        className="object-contain h-16"
+                        className="object-contain h-12 lg:h-16"
                     />
                     <Typography 
                         sx={{
-                            fontSize: 18,
                             fontWeight: 500,
                             color: theme.palette.text.primary,
                             textAlign: "center",
                             marginTop: "20px"
                         }}
-                        className="max-w-159 !tracking-[1px]"
+                        className="max-w-159 !tracking-[1px] !text-base p-5 !mt-4 lg:!text-lg lg:p-0"
                     >
                         Levvy V3 is the next-gen NFT and token lending platform on Cardano—faster, smarter, and powered by Angel Finance.
                     </Typography>
                 </div>
                 <div className="relative">
-                    <div className="absolute w-100 -left-30 bottom-38">
+                    <div className="absolute !w-[112px] top-5 sm:!w-[150px] sm:bottom-93 sm:left-5 md:bottom-[445px]] md:left-[33px] lg:-left-[58px] lg:bottom-25 lg:!w-80 xl:!w-95 2xl:-left-30 2xl:bottom-38 2xl:!w-100">
                         <img
                             src={LevvyAngel}
-                            alt="levvy mascot"
+                            alt="levvy angel"
                         />
                     </div>
-                    <div className="absolute w-46 right-47 -top-14">
+                    <div className="absolute !w-20 right-10 top-9 sm:!w-25 sm:right-20 sm:top-[26px] lg:!w-30 lg:top-[5px] lg:right-[109px]  xl:!w-40 xl:-top-[50px] xl:right-[120px] 2xl:-top-14 2xl:right-[195px] 2xl:!w-46">
                         <img
                             src={LevvyMascot}
                             alt="levvy mascot"
                         />
                     </div>
-                    <div className="w-320 mt-10">
+                    <div className="mt-20 ml-10 lg:mt-18 lg:!ml-10 lg:w-200 xl:mt-10 xl:!ml-[85px] xl:w-250 2xl:w-320">
                         <img
                             src={LevvyShowcase}
                             alt="levvy showcase"
