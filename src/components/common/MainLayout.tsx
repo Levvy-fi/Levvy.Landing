3e8f8d29240ac04ae819c23e7159d98e07c47800
Kiefer Gallego <110240392+KeeeeEf@users.noreply.github.com>
2025-06-23 23:44:59 +0800

    feat: implement new levvy section ui (#35)
    * feat: implement new levvy section ui
    
    * feat: added animations
    
    * chore: revert changes in other files

--- src/components/common/MainLayout.tsx
+++ src/components/common/MainLayout.tsx
@@ -8,8 +8,10 @@ declare module "@mui/material/styles" {
       levvy: {
         main: string;
         main_light: string;
+        main_dark: string;
         secondary: string;
         secondary_light: string;
+        secondary_dark: string;
       },
       button: {
         10: string,
@@ -31,6 +33,11 @@ declare module "@mui/material/styles" {
       200: string,
       300: string,
       400: string,    
+    },
+    levvy: {
+      primary: string;
+      secondary: string;
+      secondary_dark: string;
     }
       
   }
@@ -38,10 +45,12 @@ declare module "@mui/material/styles" {
   interface PaletteOptions {
     gradient?: {
       levvy?: {
-        main: string;
-        main_light: string;
-        secondary: string;
-        secondary_light: string;
+        main?: string;
+        main_light?: string;
+        main_dark?: string;
+        secondary?: string;
+        secondary_light?: string;
+        secondary_dark?: string;
       },
       button?: {
         10: string,
@@ -56,6 +65,11 @@ declare module "@mui/material/styles" {
         10: string,
         20: string
       }
+    },
+    levvy?: {
+      primary: string;
+      secondary: string;
+      secondary_dark: string;
     },
     chart?: {
       50: string,
@@ -89,12 +103,19 @@ const theme = createTheme({
           secondary: "#6C4900",
           disabled: "#504535"
         },
+        levvy: {
+          primary: "#FFCA60",
+          secondary: "#FF9934",
+          secondary_dark: "#FFCC66"
+        },
         gradient: {
           levvy: {
             main: "#50CEC8",
             main_light: "#F7FF19",
+            main_dark: "#CD6600",
             secondary: "#FFE77F",
-            secondary_light: "#FF700F"
+            secondary_light: "#FF700F",
+            secondary_dark: "#FFC560"
           },
           button: {
             10: "#FFE0A2",
@@ -127,8 +148,9 @@ const theme = createTheme({
           300: "#2F2920",
           400: "#3A342B",
           500: "#251F16",
+          600: "#2E2E2E",
           900: "#3F382F",
-          A100: "#12100F"
+          A100: "#101921"
         }
     },
     typography: {
